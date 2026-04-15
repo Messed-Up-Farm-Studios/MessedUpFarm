661a01e0a5a5ce623e438fe072cada04c5a7b4c6
Tyler Moessing <tmoessing@gmail.com>
2026-04-15 12:21:27 -0600

    inital FastAPI implementation

--- client/src/client.py
+++ client/src/client.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+import requests
 
 async def run():
     uri = "ws://127.0.0.1:8000/ws"
@@ -11,3 +12,31 @@ async def run():
         print(msg)
 
 asyncio.run(run())
+
+BASE_URL = "http://127.0.0.1:8000" 
+
+def send_request(method, path="/", data=None, params=None, headers=None):
+    url = BASE_URL + path
+
+    method = method.upper()
+
+    if method == "GET":
+        response = requests.get(url, params=params, headers=headers)
+    elif method == "POST":
+        response = requests.post(url, json=data, headers=headers)
+    elif method == "PUT":
+        response = requests.put(url, json=data, headers=headers)
+    elif method == "PATCH":
+        response = requests.patch(url, json=data, headers=headers)
+    elif method == "DELETE":
+        response = requests.delete(url, headers=headers)
+    else:
+        raise ValueError(f"Unsupported method: {method}")
+
+    print(response.json())
+
+# Good Post Request
+send_request("POST", "/create/player", data={"name":"Jimmy", "age":22} )
+
+# Bad Post Request
+send_request("POST", "/create/player", data={"name":123, "age":22} )
--- server/src/main.py
+++ server/src/main.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, status, BackgroundTasks
+from pydantic import BaseModel, Field
+
+players = []
+
+def save_player(player):
+    players.append(player)
+
+class Player(BaseModel):
+    name: str = Field(..., min_length=1, max_length=20, description="Player's Real Name", example="Tyler")
+    age: int = Field(ge=13, le=100, description="Players Age", example=23)
 
 app = FastAPI()
 
@@ -8,6 +18,21 @@ async def websocket_endpoint(ws: WebSocket):
     try:
         while True:
             data = await ws.receive_text()
-            await ws.send_text(f"Server got: {data}")
+            await ws.send_text(f"Server received: {data}")
     except WebSocketDisconnect:
         print("Client disconnected")
+
+@app.get("/heath")
+def health_check():
+    return {"status": "ok"}
+
+@app.post("/create/player", status_code=status.HTTP_201_CREATED, tags=["Player", "Create"])
+def create_player(player: Player, background_tasks: BackgroundTasks):
+    background_tasks.add_task(save_player, player)
+    return {"msg": "player created"}
+
+@app.get("/list/players", tags=["Player", "List"])
+def list_players():
+    return {"msg": "players list",
+            "players": players}
+
